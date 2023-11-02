0ecc3f36ce5434eb6764ac67e7917d990946d97c
junli <junli@hitotek.com>
2023-11-02 16:07:43 +0800

    1

--- src/pages/Layout/index.js
+++ src/pages/Layout/index.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
-
+import './index.module.css'
 export default class index extends Component {
   render() {
     return (
       <div>
+        <div className='aa'>
         首页布局组件
+      </div>
+      <div className="bb">
+        bbb
+      </div>
       </div>
     )
   }
